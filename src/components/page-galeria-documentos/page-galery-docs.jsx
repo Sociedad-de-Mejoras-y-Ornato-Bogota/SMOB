10b4136b41a0fdc6356588f011f033f77bea6d55
Santiago <davidsfigueroar@outlook.es>
2024-01-26 16:14:47 -0500

    modelos urbanos

--- src/components/page-galeria-documentos/page-galery-docs.jsx
+++ src/components/page-galeria-documentos/page-galery-docs.jsx
@@ -1,19 +1,44 @@
 import "./styles.css"
+import { useState } from "react";
 const Page_galery_docs = (props) => {
-    let documents = props.urls_content
+    let documents = props.urls_content;
+    let paragraphs = props.description
+
+    let [selected, setSelected] = useState(0)
     return (
         <div className="page_galery_docs">
-            <section className='title_section_galery'>
-                <h2 className='title_page'>{props.title}</h2>
+            <section className='title_section_galery' >
+                <h2 className='title_page-galery'>{props.title}</h2>
+                {paragraphs?.map((paragraph, index) => {
+                    return (
+                        <p className="text_description" key={index}>
+                            {paragraph}
+                        </p>
+                    )
+                })}
+
+            </section>
+            <section style={{ width: "100%", display: "flex", justifyContent: "center" , margin:"1rem"}} className="menu_galery">
+                {documents.map((document, index) => {
+                    return (
+                        <button key={index}  className={`filtro-publicaciones ${selected === index ? "button-selected" : ""}`}   style={{ border: "none", margin: "3rem", color: "#91583B", width:"33.3vw", display:"flex", justifyContent:"center", padding:"1rem" }} onClick={()=>{
+                            setSelected(index)
+                        }}>
+                            {document.title} 
+                        </button>
+                    )
+                })}
+            </section>
+            <section>
             </section>
             <section className='content_galery'>
                 {documents?.map((document, index) => {
-                    return (
+                    if(selected == index){                    return (
                         <article key={index}>
                             <iframe className="galery_content" src={document.url} ></iframe>
                             <span className="title_galery_content">{document.title}</span>
                         </article>
-                    )
+                    )}
                 })}
 
             </section>
