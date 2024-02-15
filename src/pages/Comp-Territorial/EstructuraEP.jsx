0cba1e54874b5e5b2eaecfab5f3e7853b835b20c
David-Diaz-Gomez <davidgerdiaz777@gmail.com>
2024-02-15 11:36:23 -0500

    EP

---
+++ src/pages/Comp-Territorial/EstructuraEP.jsx
@@ -0,0 +1,10 @@
+import Page_tipe_full from '../../components/page-tipe-full/page-tipe-full'
+function EstructuraEP() {
+  return (
+    <>
+    <Page_tipe_full title="Estructura Ecológica Principal" content_url ="https://geoapps.esri.co/portal/apps/experiencebuilder/experience/?id=0675067dccb64094afe426f5f598a6b8"/>
+    </>
+  );
+}
+
+export default EstructuraEP;
