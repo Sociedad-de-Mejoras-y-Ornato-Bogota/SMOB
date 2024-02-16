1cea7231f30c4ba756f2617a5066fa3afe1b20a3
David-Diaz-Gomez <davidgerdiaz777@gmail.com>
2024-02-16 09:42:51 -0500

    Audios

--- src/components/page-principal/page-principal.jsx
+++ src/components/page-principal/page-principal.jsx
@@ -1,11 +1,73 @@
-import './styles.css'
+import React, { useState, useEffect, useRef } from 'react';
+import './styles.css';
+
 const Page_principal = (props) => {
-    return (
-        <>
-            <div className="page_principal">
-                <section className='left_section'>
-                    <img src={props.url_imagen} className="img_componente" width="150" height="150" alt='Imagen circulo por componente' useMap="#examplemap"></img>
-                    {props.areas && (
+  const audioRef = useRef(null);
+  const [reproduciendo, setReproduciendo] = useState(false);
+  const [audioCargado, setAudioCargado] = useState(false);
+
+  const handlePlayPause = async () => {
+    const audioElement = audioRef.current;
+
+    if (!audioCargado) {
+      await cargarAudio(); // Esperar hasta que el audio esté completamente cargado
+    }
+
+    if (audioElement && audioElement.paused) {
+      audioElement.play().catch(error => {
+        console.error('Error al reproducir audio:', error);
+      });
+    } else if (audioElement) {
+      audioElement.pause();
+    }
+  };
+
+  const cargarAudio = async () => {
+    const audioElement = audioRef.current;
+
+    if (audioElement && audioElement.readyState < 2) {
+      // El estado 2 indica que el audio está completamente cargado
+      await new Promise((resolve) => {
+        const handleCanPlayThrough = () => {
+          setAudioCargado(true);
+          resolve();
+        };
+
+        audioElement.addEventListener('canplaythrough', handleCanPlayThrough);
+
+        // Si el audio ya está cargado, resolver de inmediato
+        if (audioElement.readyState >= 2) {
+          setAudioCargado(true);
+          resolve();
+        }
+      });
+    }
+  };
+
+  useEffect(() => {
+    const audioElement = audioRef.current;
+
+    if (audioElement) {
+      const handleStateChange = () => {
+        setReproduciendo(!audioElement.paused);
+      };
+
+      audioElement.addEventListener('play', handleStateChange);
+      audioElement.addEventListener('pause', handleStateChange);
+
+      return () => {
+        audioElement.removeEventListener('play', handleStateChange);
+        audioElement.removeEventListener('pause', handleStateChange);
+      };
+    }
+  }, []);
+
+  return (
+    <>
+      <div className="page_principal">
+        <section className='left_section'>
+          <img src={props.url_imagen} className="img_componente" width="150" height="150" alt='Imagen circulo por componente' useMap="#examplemap" />
+          {props.areas && (
                         <map name="examplemap">
                         {
                             props.areas.map((area, index) => {
@@ -14,15 +76,20 @@ const Page_principal = (props) => {
                         }
                         </map>
                     )}
-                    
-                </section>
-                <section className='right_section'>
-                    <p className="name_component">{props.name_component}</p>
-                    <p className="description_component">{props.description_component}</p>
-                </section>
+        </section>
+        <section className='right_section'>
+          {props.audio && (
+            <div className="button-container">
+              <button onClick={() => handlePlayPause()}>{reproduciendo ? '⫾⫾' : '⧐'}</button>
+              <audio ref={audioRef} src={props.audio} type="audio/mp3" controls={false} autoPlay />
             </div>
-        </>
-    )
-}
+          )}
+          <p className="name_component">{props.name_component}</p>
+          <p className="description_component">{props.description_component}</p>
+        </section>
+      </div>
+    </>
+  );
+};
 
-export default Page_principal
+export default Page_principal;
--- src/pages/Comp-Bienestar/Ordenamiento.jsx
+++ src/pages/Comp-Bienestar/Ordenamiento.jsx
@@ -21,24 +21,12 @@ export default function Bienestar() {
 
   return (
     <>
-      <Page_principal url_imagen="https://smob-storage.s3.us-east-2.amazonaws.com/recursosSMOB/Bienestar/Componente Bienestar.png" name_component="Componente Bienestar" description_component="Los instrumentos de ordenamiento territorial que históricamente se han
-concebido, tienen el propósito de hacer de la Bogotá del presente una metrópolis
-moderna capaz de albergar a millones de colombianos. Es imprescindible que la
-ciudad les proporcione servicios públicos, salud, vivienda, movilidad y vías,
-espacios públicos, esparcimiento y seguridad, y condiciones de habitabilidad
-apropiadas, entre otros elementos vitales para lograr una vida plena y digna. En
-este proceso, la capital del país ha sido entendida de diversas maneras, con
-diversas ópticas, como lo reflejan las normas, acuerdos y decretos que en cada
-etapa han sido desarrollados.
-En consecuencia, es indispensable que quien estudia estos asuntos
-conozca el proceso histórico que atravesó Bogotá para lograr albergar a millones
-y millones de personas a lo largo de los últimos 100 años. En este caso específico,
-se trata de la historia de la planeación en Bogotá, de cómo en cada momento de
-la vida de la ciudad se tomaron las providencias y se dispusieron los recursos
-técnicos, humanos e institucionales para hacerles frente a los inmensos desafíos
-que surgían a cada paso. En cuáles elementos se hizo el énfasis, qué visiones de
-ciudad se privilegiaron, qué tipo de futuro se entreveía o era dable anticipar, son
-el tipo de preguntas que se busca responder en esta sección." />
+      <Page_principal url_imagen="https://smob-storage.s3.us-east-2.amazonaws.com/recursosSMOB/Bienestar/Componente Bienestar.png" name_component="Componente Bienestar" 
+      description_component="Los instrumentos de ordenamiento territorial que históricamente se han concebido, tienen el propósito de hacer de la Bogotá del presente una metrópolis moderna capaz de albergar a millones de colombianos. Es imprescindible que la ciudad les proporcione servicios públicos, salud, vivienda, movilidad y vías, espacios públicos, esparcimiento y seguridad, y condiciones de habitabilidad apropiadas, entre otros elementos vitales para lograr una vida plena y digna. En este proceso, la capital del país ha sido entendida de diversas maneras, con diversas ópticas, como lo reflejan las normas, acuerdos y decretos que en cada etapa han sido desarrollados.       
+      En consecuencia, es indispensable que quien estudia estos asuntos conozca el proceso histórico que atravesó Bogotá para lograr albergar a millones y millones de personas a lo largo de los últimos cien años.
+      En este caso específico, se trata de la historia de la planeación en Bogotá, de cómo en cada momento de la vida de la ciudad se tomaron las providencias y se dispusieron los recursos técnicos, humanos e institucionales para hacerles frente a los inmensos desafíos que surgían a cada paso. En cuáles elementos se hizo el énfasis, qué visiones de ciudad se privilegiaron, qué clase de futuro se entreveía o era dable anticipar, son el tipo de preguntas que se busca responder en esta sección."
+      audio='https://smob-storage.s3.us-east-2.amazonaws.com/recursosSMOB/recursos_15_02_2024/Audio Componente bienestar.mp3' 
+      />
 
 
       {windowWidth > 800 && (
