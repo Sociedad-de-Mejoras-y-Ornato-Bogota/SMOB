0d7515082faef7155ad7830e76b6bcd07c58ed2f
ChristianGalindo10 <cygc1@hotmail.com>
2024-03-11 11:59:01 -0500

    FIX: updated dashboard

--- src/pages/Comp-Bienestar/Estructura/Vivienda/Desarrollos/estudio.jsx
+++ src/pages/Comp-Bienestar/Estructura/Vivienda/Desarrollos/estudio.jsx
@@ -2,7 +2,7 @@ import Page_tipe_full from "../../../../../components/page-tipe-full/page-tipe-f
 export default function Estudios() {
     return (
         <>
-            <Page_tipe_full title="Estudio de caso de barrios"  content_url="https://www.arcgis.com/apps/dashboards/a3f35c2b09e6471cb3a650283b1c1cdd"/>
+            <Page_tipe_full title="Estudio de caso de barrios"  content_url="https://www.arcgis.com/apps/dashboards/0b1093621bdd45d589dbe09ede1c73be"/>
         </>
     )
 }
