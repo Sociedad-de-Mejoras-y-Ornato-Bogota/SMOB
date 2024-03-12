805ddce5ada07e8ee68db5339d505b8cba784ccb
ChristianGalindo10 <cygc1@hotmail.com>
2024-03-12 11:30:56 -0500

    FEAT: Added PDFs

--- src/pages/Comp-Poblacional/Conflicto-Civilidad/Demografia.jsx
+++ src/pages/Comp-Poblacional/Conflicto-Civilidad/Demografia.jsx
@@ -1,8 +1,15 @@
-import Pronto from "../../../components/Not found/ComingSoon"
 const Demografiavu = () =>{
     return(
         <>
-        <Pronto/>
+         <div style={{ minHeight: "65vh", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+                <section className='title_section'>
+                    <h2 className='title_page'>Demografía, violencia y urbanización</h2>
+                </section>
+               <div style={{width: '100%'}}>
+                    <br/>
+                    <iframe src="https://smob-storage.s3.us-east-2.amazonaws.com/publicaciones/Demografi%CC%81a+violencia+y+urbanizacio%CC%81n_SMOB.pdf"  style={{width:"60%", height:"80vh", display: 'block', margin: '0 auto'}}/>
+                </div>
+            </div>
         </>
     )
 }
