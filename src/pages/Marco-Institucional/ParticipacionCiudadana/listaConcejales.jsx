f28aeb20828af73c25926f4fc7a3063c73da2030
murvn77 <mauren.rivera.16@hotmail.com>
2024-03-13 10:43:08 -0500

    FIX: Lista Concejo

--- src/pages/Marco-Institucional/ParticipacionCiudadana/listaConcejales.jsx
+++ src/pages/Marco-Institucional/ParticipacionCiudadana/listaConcejales.jsx
@@ -2,7 +2,7 @@ import Page_tipe_full from "../../../components/page-tipe-full/page-tipe-full"
 export default function ListaConcejales() {
     return (
         <>
-            <Page_tipe_full title="Lista de concejo de Bogotá"  content_url="https://geoapps.esri.co/portal/apps/dashboards/ac59d4dab7c74576ad432359e2c3cd0a"/>
+            <Page_tipe_full title="Lista de Concejo de Bogotá"  content_url="https://geoapps.esri.co/portal/apps/dashboards/ac59d4dab7c74576ad432359e2c3cd0a"/>
         </>
     )
 }
