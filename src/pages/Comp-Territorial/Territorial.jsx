94bee8e31b5c14e959f8c0fa64ab3e78faf692d4
ChristianGalindo10 <cygc1@hotmail.com>
2024-03-13 09:33:15 -0500

    FIX: Imagen componente territorial

--- src/pages/Comp-Territorial/Territorial.jsx
+++ src/pages/Comp-Territorial/Territorial.jsx
@@ -8,7 +8,7 @@ const Territorial = () => {
     }
     return (
         <>
-                    <Page_principal url_imagen="https://smob-storage.s3.us-east-2.amazonaws.com/circulos/Componente Territorial/COMPONENTE TERRITORIO-01.png" name_component="Componente territorial"
+                    <Page_principal url_imagen="https://smob-storage.s3.us-east-2.amazonaws.com/circulos/Componente+Territorial/Final+componente+territorial.png" name_component="Componente territorial"
                      description_component="El territorio donde fue establecida la pequeña aldea que con el paso de los años llegaría a ser la capital de la nación -250 mil hectáreas de uno de los suelos agroecológicos más valiosos del país, a 2600 metros de altitud, pero en el centro del trópico-, se expandió y se transformó incesantemente, a medida que su población aumentaba y que ella misma lo afectaba. Se trató de un proceso de siglos, en el cual la región tuvo diversas facetas políticas y administrativas, a tono con los avatares de nuestra historia nacional. En este panorama, son de importancia mayor, asuntos como la riqueza hídrica, el sistema ambiental, la evolución de la cartografía (incluyendo el componente digital y la geo referenciación), y los índices y variables claves del crecimiento poblacional."
                      audio='https://smob-storage.s3.us-east-2.amazonaws.com/recursosSMOB/recursos_15_02_2024/Audio Componente territorial.mp3'
                      areas={
