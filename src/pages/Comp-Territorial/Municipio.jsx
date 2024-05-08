6cab815c0d1dde31001645c224405adf07d10291
ChristianGalindo10 <cygc1@hotmail.com>
2024-05-08 08:47:37 -0500

    FEAT: EB Municipio

--- src/pages/Comp-Territorial/Municipio.jsx
+++ src/pages/Comp-Territorial/Municipio.jsx
@@ -1,8 +1,8 @@
-import Pronto from '../../components/Not found/ComingSoon'
+import Page_tipe_full from '../../components/page-tipe-full/page-tipe-full';
 export default function Municipio() {
     return (
         <>
-            <Pronto />
+        <Page_tipe_full title="Municipio, estructura rural, y región metropolitana" content_url ="https://experience.arcgis.com/experience/92948c9b7b0944e0bbf5d2b528b8c029/"/>
         </>
-    )
+      );
 }
