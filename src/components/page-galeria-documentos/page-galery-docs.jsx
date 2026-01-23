f8e721f3792f79123c73ebbf20bfe645fd4701aa
sosmejorasbogota <admisnitrador@sosmejorasbogota.com>
2026-01-23 09:35:35 -0500

    Ajuste Bogotá Futura

--- src/components/page-galeria-documentos/page-galery-docs.jsx
+++ src/components/page-galeria-documentos/page-galery-docs.jsx
@@ -21,7 +21,7 @@ const Page_galery_docs = (props) => {
             <section style={{ width: "100%", display: "flex", justifyContent: "center" , margin:"1rem"}} className="menu_galery">
                 {documents.map((document, index) => {
                     return (
-                        <button key={index}  className={`filtro-publicaciones ${selected === index ? "button-selected" : ""}`}   style={{ border: "none", margin: "3rem", color: "#91583B", width:"33.3vw", display:"flex", justifyContent:"center", padding:"1rem" }} onClick={()=>{
+                        <button key={index}  className={`filtro-publicaciones ${selected === index ? "button-selected" : ""}`}   style={{ border: "none", margin: "3rem", color: "#91583B", width:"33.3vw", display:"flex", justifyContent:"center", alignItems:"center", textAlign:"center",padding:"1rem" }} onClick={()=>{
                             setSelected(index)
                         }}>
                             {document.title} 
