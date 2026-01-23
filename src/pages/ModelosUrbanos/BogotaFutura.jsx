247ccd5082bf7556cabc4f4fce794ebfd95a2635
sosmejorasbogota <admisnitrador@sosmejorasbogota.com>
2026-01-23 09:57:57 -0500

    Ajuste Bogotá Futura v2

--- src/pages/ModelosUrbanos/BogotaFutura.jsx
+++ src/pages/ModelosUrbanos/BogotaFutura.jsx
@@ -7,13 +7,13 @@ const BogotaFutura = () => {
             <Page_galery_docs title="Herramienta para el análisis y comprensión del POT (Decreto 555 de 2021)"
                 description={[
                     "La herramienta práctica que se presenta a continuación, ayuda al análisis y comprensión del Plan de Ordenamiento Territorial adoptado mediante el Decreto Distrital 555 de 2021. ",
-                    "Está constituido por 3 elementos. El primero es un completo glosario del POT, el segundo compila sus políticas, objetivos, estrategias y planes, y el tercero compendia el conjunto de normas urbanísticas aplicables al Distrito Capital.",
-                    "Cada uno de los 3 elementos está estructurado en torno a cuatro componentes esenciales: el general, el poblacional, el territorial y el de bienestar, que interactúan dinámicamente en la construcción del modelo de ciudad del presente y del futuro. " ]} urls_content={
+                    "Está constituido por 4 elementos. El primero es un completo glosario del POT, el segundo compila sus políticas, objetivos, estrategias y planes, el tercero compendia el conjunto de normas urbanísticas aplicables al Distrito Capital, y el cuarto la cartografía principal de los componentes General, Urbano, y Rural.",
+                    "Cada uno de los elementos está estructurado en torno a cuatro componentes esenciales: el general, el poblacional, el territorial y el de bienestar, que interactúan dinámicamente en la construcción del modelo de ciudad del presente y del futuro. " ]} urls_content={
                         [
                             { url: "https://smob-storage.s3.us-east-2.amazonaws.com/modelos+de+ciudad/1.+POT+Definiciones+2.pdf", title: "Definiciones" },
-                            { url: "https://smob-storage.s3.us-east-2.amazonaws.com/modelos+de+ciudad/2.+POT_Pol%2CObj%2CEst%2CPla+2.pdf", title: "Politicas, objetivos, estrategias y planes" },
-                            { url: "https://smob-storage.s3.us-east-2.amazonaws.com/modelos+de+ciudad/3.%2BPOT%2BNormativa+2.pdf", title: "Normas" },
-                            { url: "https://smob-storage.s3.us-east-2.amazonaws.com/modelos+de+ciudad/4.+Cartograf%C3%ADa+POT+2021.pdf", title: "Cartografía" }]} />
+                            { url: "https://smob-storage.s3.us-east-2.amazonaws.com/modelos+de+ciudad/2.+POT_Pol%2CObj%2CEst%2CPla+2.pdf?v=20260123", title: "Politicas, objetivos, estrategias y planes" },
+                            { url: "https://smob-storage.s3.us-east-2.amazonaws.com/modelos+de+ciudad/3.%2BPOT%2BNormativa+2.pdf?v=20260123", title: "Normas" },
+                            { url: "https://smob-storage.s3.us-east-2.amazonaws.com/modelos+de+ciudad/4.+Cartograf%C3%ADa+POT+2021.pdf?v=20260123", title: "Cartografía" }]} />
         </>
     )
 }
